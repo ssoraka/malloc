8f0d13c9c99afe324bcf8df889b7d00b3764fb05
ssoraka <t89043139690@mail.ru>
2020-10-09 00:15:16 +0300

    clear file

--- src/malloc.c
+++ src/malloc.c
@@ -4,87 +4,6 @@
 
 #include "../includes/malloc.h"
 
-void	init_pages(t_pages *pages, char *mem, int page_size, int page_count)
-{
-	int i;
-	t_page *page;
-
-	i = 0;
-	pages->head = NULL;
-	page = (t_page *)mem;
-	while (i < page_count)
-	{
-		ft_bzero((void *)page, sizeof(t_page));
-//		page->mem = mem + sizeof(t_page);
-		page->size = page_size - sizeof(t_page);
-		page->next = (t_page *)(mem + page_size);
-		page++;
-		mem = mem + page_size;
-		i++;
-	}
-	pages->last = page;
-	pages->count = page_count;
-}
-
-int		ft_get_page_size(int type)
-{
-	if (type == TINY)
-		return (getpagesize() * TINY_SIZE);
-	if (type == SMALL)
-		return (getpagesize() * SMALL_SIZE);
-}
-
-
-
-/*
-t_store		*create_new_store()
-{
-	t_store	*mem;
-	int size;
-	int tiny_size;
-	int small_size;
-
-	tiny_size = (ft_get_page_size(TINY) + sizeof(t_page)) * 100;
-	small_size = (ft_get_memory_size_on_page(SMALL) + sizeof(t_page)) * 100;
-	size = sizeof(t_store) + tiny_size + small_size;
-	mem = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
-	if (!mem)
-		return (NULL);
-	init_pages(&mem->p[TINY], (char *)(mem + 1),
-				TINY_SIZE + sizeof(t_page), 100);
-	init_pages(&mem->p[SMALL], (char *)(mem + 1) + tiny_size,
-				SMALL_SIZE + sizeof(t_page), 100);
-	mem->p[LARGE] = (t_pages){NULL, NULL, 0};
-	return (mem);
-}
-
-
-t_store		*get_store()
-{
-	static t_store	*mem;
-
-	if (!mem)
-		mem = create_new_store();
-	return (mem);
-}
-
-t_page *find_page(t_pages *pages, size_t size)
-{
-	t_page *page;
-	int i;
-
-	page = pages->head;
-	i = 0;
-	while (i < pages->count && page) {
-		if (page->size - page->used > size)
-			return (page);
-		page = page->next;
-		i++;
-	}
-	return (insert_new_page(pages, ));
-}
-*/
-
 /*
  * так, я начал неправильно
  * нужно сначала попытаться вставить в последний кадр новый блок памяти
@@ -102,8 +21,6 @@ t_page *find_page(t_pages *pages, size_t size)
  * надо навставлять заглушек, вместо выделения памяти
  * и отладить все изолированно
  */
-
-
 
 
 t_root	*get_root()
@@ -192,8 +109,5 @@ void	*ft_malloc(size_t size)
 		return (NULL);
 	insert_page_in_root(page);
 	ptr = alloc_mem(page, &page->alloc, size);
-
-
-
 	return (ptr);
 }
