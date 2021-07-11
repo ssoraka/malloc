17b02b172f5c5e8138848320d167d0dc6eefc6e2
root <ssoraka@student.21-school.ru>
2021-07-11 17:43:12 +0300

    add tests

---
+++ tests/test4.c
@@ -0,0 +1,33 @@
+
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test4.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ssoraka <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/01 22:18:13 by vsteffen          #+#    #+#             */
+/*   Updated: 2018/08/01 22:18:15 by vsteffen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+void	print(char *s)
+{
+	write(1, s, strlen(s));
+}
+
+int		main(void)
+{
+	char	*addr;
+
+	addr = malloc(16);
+	free(NULL);
+	free((void*)addr + 5);
+	if (realloc((void*)addr + 5, 10) == NULL)
+		print("Bonjours\n");
+	return (0);
+}
